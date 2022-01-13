a188b00ebcced6299c341da811bdfbb9c532368b
JeromeV70 <villani.jerome@gmail.com>
2022-01-13 16:43:10 +0100

    tests

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,26 +1,27 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Menu/>
+  <Connexion/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Menu from './components/menu.vue'
+import Connexion from './components/connexion.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Menu, Connexion
   }
 }
 </script>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Ubuntu, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  display:grid;
+  grid-template-columns: 1fr,1fr;
+  grid-template-rows:auto,auto,auto;
 }
 </style>
---
+++ front/src/components/menu.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <nav>
+            <a href="/"><img alt="logo groupomania" src="../assets/icon-left-font-monochrome-black.svg"></a>
+            <a href="/connexion">Connexion</a>
+            <a href="/inscription">Inscription</a>
+        </nav>
+    </div>
+</template>
+<style>
+nav
+{
+    display:grid;
+    grid-column: 1/2;
+    grid-row: 1/2;
+    border:1px solid black;
+}
+a img
+{
+    width:10em;
+}
+</style>
